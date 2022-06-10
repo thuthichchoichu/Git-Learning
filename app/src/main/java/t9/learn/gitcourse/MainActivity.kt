86f20cb164393abcd586a1398616e11082fa7d20
work.thunguyen0608@gmail.com <1Iloveyou1@>
2022-06-10 10:31:25 +0700

    first init

---
+++ app/src/main/java/t9/learn/gitcourse/MainActivity.kt
@@ -0,0 +1,11 @@
+package t9.learn.gitcourse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
